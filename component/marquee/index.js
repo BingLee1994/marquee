e4ae6311a91bfa93d32fdd1a36bce09c48b32008
LeeYabing <791188716@qq.com>
2020-02-29 14:36:19 +0800

    Enhance performance. 优化性能，仅当节点在视区时才进行滚动

--- component/marquee/index.js
+++ component/marquee/index.js
@@ -17,6 +17,18 @@ const maxSpeed = 250;
 const widthWatcherInterval = 100;
 const debounceDelay = 1000;
 
+function setTranslateX(element, x) {
+    element.style.transform = `translate(${x}px, 0px)`;
+    element._currentPosition = x;
+}
+
+function moveLeft(element, offset) {
+    let position = parseFloat(element._currentPosition || 0);
+    let newPosition = position - offset;
+
+    return newPosition;
+}
+
 export default class extends React.PureComponent{
     componentDidUpdate() {
         this.startAnimationTimer();
@@ -95,47 +107,97 @@ export default class extends React.PureComponent{
         tail.innerHTML = this.props.children;
 
         let { gapWidth, speed, delay } = this.getOptionFromProps();
-        let initPosition = txtWidth + gapWidth;
-        this._moveLeft(tail, -initPosition);
+        let initPosition = containerWidth + gapWidth;
+        setTranslateX(tail, moveLeft(tail, -containerWidth));
+        tail.style.visibility = "hidden";
+        let boundary = txtWidth - containerWidth;
+
+        let widthDetail = {
+            boundary,
+            containerWidth,
+            txtWidth,
+            initPosition,
+            gapWidth
+        }
 
         this.starterTimer = setTimeout(() => {
             this.animationTimer = setInterval(() => {
-                this.textAnimate(txt, tail, initPosition, speed);
+                this.textAnimate(txt, tail, speed, widthDetail);
             }, 1000 / 60);
         }, delay);
     }
 
-    _setTranslateX(element, x) {
-        element.style.transform = `translate(${x}px, 0px)`;
-        element._currentPosition = x;
-    }
-
-    _moveLeft(element, offset) {
-        let position = parseFloat(element._currentPosition || 0);
-        let newPosition = position - offset;
-        this._setTranslateX(element, newPosition);
-        element._currentPosition = newPosition;
-
-        return newPosition;
-    }
-
-    textAnimate(txt, tail, initialPosition, speed) {
+    textAnimate(txt, tail, speed, widthDetail) {
         let step = speed / 60;
+        let {boundary, initPosition, txtWidth} = widthDetail;
         if (!txt || !tail) {
             return
         }
 
         let oldTxtPosition = txt._currentPosition || 0,
-            oldTailPosition = tail._currentPosition || 0,
-            newTxtPosition = this._moveLeft(txt, step),
-            newTailPosition = this._moveLeft(tail, step);
+            newTxtPosition = moveLeft(txt, step),
+            isTxtShouldUpdate = true;
 
-        if (oldTxtPosition > 0 && newTxtPosition <= 0) {
-            this._setTranslateX(tail, initialPosition);
+        let oldTailPosition = tail._currentPosition || 0,
+            newTailPosition = moveLeft(tail, step),
+            isTailShouldUpdate = true;
+
+        if (oldTxtPosition >= -boundary && newTxtPosition <= -boundary) {
+            //tail节点即将进入父容器视区
+            tail.style.visibility = 'visible';
+            setTranslateX(tail, initPosition);
         }
 
-        if (oldTailPosition > 0 && newTailPosition <= 0) {
-            this._setTranslateX(txt, initialPosition);
+        if (newTxtPosition <= -boundary) {
+            //tail节点已经进入父容器视区
+            isTailShouldUpdate = true;
+        }
+
+        if (oldTailPosition >= -boundary && newTailPosition <= -boundary) {
+            //txt节点即将进入父容器视区
+            txt.style.visibility = 'visible';
+            setTranslateX(txt, initPosition);
+        }
+
+        if (newTailPosition <= -boundary) {
+            //txt节点已经进入父容器视区
+            isTxtShouldUpdate = true;
+        }
+
+        //txt彻底从屏幕左侧消失
+        if (oldTxtPosition <= -txtWidth) {
+            isTxtShouldUpdate = false;
+        }
+
+        //tail彻底从屏幕左侧消失
+        if (oldTailPosition <= -txtWidth) {
+            isTailShouldUpdate = false;
+        }
+
+        //txt占据整个父容器视区
+        if (oldTxtPosition < 0  && oldTxtPosition >= -boundary) {
+            isTailShouldUpdate = false;
+        }
+
+        //tail占据整个父容器视区
+        if (oldTailPosition < 0 && oldTailPosition >= -boundary) {
+            isTxtShouldUpdate = false;
+        }
+
+        let txtVisibility = txt.style.visibility,
+            tailVisibility = tail.style.visibility
+
+        if (isTailShouldUpdate) {
+            (tailVisibility !== 'visible') && (tail.style.visibility = "visible");
+            setTranslateX(tail, newTailPosition);
+        } else {
+            (tailVisibility !== 'hidden') && (tail.style.visibility = "hidden");
+        }
+        if (isTxtShouldUpdate) {
+            (txtVisibility !== 'visible') && (txt.style.visibility = "visible");
+            setTranslateX(txt, newTxtPosition);
+        } else {
+            (txtVisibility !== 'hidden') && (txt.style.visibility = "hidden");
         }
     }
 
